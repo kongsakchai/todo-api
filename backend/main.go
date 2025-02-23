ec49e7917842c6ccf3a02e5cddbc09bfa5f457cf
kongsakchai <ksakchai.dev@gmail.com>
2025-02-24 04:31:02 +0700

    :tada: implement graceful shutdown

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 	"todo-api/app"
 	"todo-api/config"
 	"todo-api/database"
@@ -13,15 +19,48 @@ func main() {
 	db, close := database.NewPostgres(cfg)
 	defer close()
 
-	s := todo.NewStorage(db)
-	h := todo.NewHandler(s)
-
 	r := app.NewGinRouter()
-	r.GET("/todos", h.Todos)
-	r.GET("/todos/:id", h.Todo)
-	r.POST("/todos", h.Create)
-	r.PUT("/todos/:id", h.Update)
-	r.DELETE("/todos/:id", h.Delete)
 
-	r.Run(fmt.Sprintf(":%s", cfg.Port))
+	{
+		s := todo.NewStorage(db)
+		h := todo.NewHandler(s)
+		r.GET("/todos", h.Todos)
+		r.GET("/todos/:id", h.Todo)
+		r.POST("/todos", h.Create)
+		r.PUT("/todos/:id", h.Update)
+		r.DELETE("/todos/:id", h.Delete)
+	}
+
+	serv := http.Server{
+		Addr:              fmt.Sprintf(":%s", cfg.Port),
+		Handler:           r,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	idleConnsClosed := make(chan struct{})
+	go gracefulShutdown(&serv, close, idleConnsClosed)
+
+	fmt.Println("Server started at", cfg.Port)
+	if err := serv.ListenAndServe(); err != http.ErrServerClosed {
+		log.Println(err)
+	}
+
+	<-idleConnsClosed
+}
+
+func gracefulShutdown(serv *http.Server, dbClose func(), idle chan struct{}) {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer stop()
+	<-ctx.Done()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := serv.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	}
+	dbClose()
+
+	fmt.Println("Server shutdown gracefully")
+	close(idle)
 }
